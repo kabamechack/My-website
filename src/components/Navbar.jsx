53acef50088b82d46387f3c1561814fc9b51f13d
kabamechack <139652522+kabamechack@users.noreply.github.com>
2024-08-08 21:37:00 +0200

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaSearch, FaShoppingCart } from 'react-icons/fa';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='navbar-logo'>
+        <Link to="/">Foodie's</Link>
+      </div>
+      <ul className='navbar-links'>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/facility">Facility</Link></li>
+      </ul>
+      <div className='navbar-icons'>
+        <FaSearch className="navbar-icon" />
+        <FaShoppingCart className="navbar-icon" />
+        <Link to="/login" className="login-button">Login</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Home.css';
+import taste from '../assets/taste.avif';
+import { FaArrowRight } from 'react-icons/fa';
+
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <h1>The Taste of Traditions's with a Modern Twist</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Donec ac metus nec lacus consequat aliquam non non est. Suspendisse potenti
+        Donec ac metus nec lacus consequat aliquam non non est. Suspendisse potenti
+        Donec ac metus nec lacus consequat aliquam non non est. Suspendisse potenti.</p>
+        <div className="home-buttons">
+          <button className="btn get-started">Get Started</button>
+          <button className="btn see-menu">
+            See Our Menu <FaArrowRight className="arrow-icon" />
+          </button>
+        </div>
+      </div>
+      <div className="home-image">
+        <img src={taste} alt="Foodie's Delight" />
+      </div>
+    </div>
+  );
+}
+export default Home;
